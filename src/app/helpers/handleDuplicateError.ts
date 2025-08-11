082ad69dac49896fb90882b7fe84cc931db93e42
Israfil Hossen <sisrafilss@gmail.com>
2025-08-11 15:35:29 +0600

    part-6 complete

--- src/app/helpers/handleDuplicateError.ts
+++ src/app/helpers/handleDuplicateError.ts
@@ -3,8 +3,9 @@ import { TGenericErrorResponse } from "../interfaces/error.types";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handleDuplicateError = (err: any): TGenericErrorResponse => {
   const matchedArray = err.message.match(/"([^"]*)"/);
+  const duplicateField = matchedArray ? matchedArray[1] : "This field";
   return {
     statusCode: 400,
-    message: `${matchedArray[1]} already exists!`,
+    message: `${duplicateField} already exists!`,
   };
 };
---
+++ src/app/modules/booking/booking.route.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { Role } from "../user/user.interface";
+import { BookingController } from "./booking.controller";
+import {
+  createBookingZodSchema,
+  updateBookingStatusZodSchema,
+} from "./booking.validate";
+
+const router = Router();
+
+// api/v1/booking
+router.post(
+  "/",
+  checkAuth(...Object.values(Role)),
+  validateRequest(createBookingZodSchema),
+  BookingController.createBooking
+);
+
+// api/v1/booking
+router.get(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  BookingController.getAllBookings
+);
+
+// api/v1/booking/my-bookings
+router.get(
+  "/my-bookings",
+  checkAuth(...Object.values(Role)),
+  BookingController.getUserBookings
+);
+
+// api/v1/booking/booingId
+router.get(
+  "/:bookingId",
+  checkAuth(...Object.values(Role)),
+  BookingController.getSingleBooking
+);
+
+// api/v1/booking/bookingId/status
+router.patch(
+  "/:bookingId/status",
+  validateRequest(updateBookingStatusZodSchema),
+  checkAuth(...Object.values(Role)),
+  BookingController.updateBookingStatus
+);
+
+export const bookingRoutes = router;
